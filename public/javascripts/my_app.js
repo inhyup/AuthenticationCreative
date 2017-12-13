af9a20694cf23539f42d25fdc33a003032bc8090
inhyup <hyup3569@gmail.com>
2017-12-12 23:06:02 -0700

    modify chat

--- public/javascripts/my_app.js
+++ public/javascripts/my_app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ["firebase"]).
+angular.module('myApp', []).
 controller('myController', ['$scope', '$http',
                             function($scope, $http) {
   $http.get('/user/profile')
@@ -10,14 +10,4 @@ controller('myController', ['$scope', '$http',
     $scope.user = {};
     $scope.error = data;
   });
-
-  var ref = firebase.database().ref().child("messages");
-  $scope.chats = $http(ref);
-  $scope.update = function(user) {
-      var newmessage = {from:user.name || "anonymous",body:user.chat};
-      console.log(newmessage);
-      $scope.chats.$add(newmessage);
-      user.chat = "";
-  }
-
 }]);
