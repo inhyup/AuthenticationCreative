fa8ecc37c156dfa7bfce80e1874c43c0a80ae9fc
inhyup <hyup3569@gmail.com>
2017-12-13 00:18:10 -0700

    check

--- public/javascripts/my_app.js
+++ public/javascripts/my_app.js
@@ -11,16 +11,53 @@ controller('myController', ['$scope', '$http',
     $scope.error = data;
   });
 
-  $scope.Todos = [
-    
-  ];
-
-  $scope.AddTodo = function() {
-    $scope.Todos.push({text:$scope.TodoText, done:false});
-    $scope.TodoText='';
-  };
-
-
-
 }]);
+
+
+$(document).ready(function(){
+  $("#postComment").click(function(){
+      var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
+      jobj = JSON.stringify(myobj);
+      $("#json").text(jobj);
+
+      var url = "comment";
+      $.ajax({
+           url:url,
+           type: "POST",
+           data: jobj,
+           contentType: "application/json; charset=utf-8",
+           success: function(data,textStatus) {
+                 $("#done").html(textStatus);
+           }
+      })
+  });
+
+
+  $("#getComments").click(function() {
+    $.getJSON('comment', function(data) {
+      console.log(data);
+      var everything = "<ul>";
+      for(var comment in data) {
+        com = data[comment];
+        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+      }
+      everything += "</ul>";
+      $("#comments").html(everything);
+    })
+  });
+
+  
+  $('#deleteComments').click(function() {
+    var url = "deleteComments";
+    $.ajax({
+      url: url,
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      success: function(data, textStatus) {
+        $("#done").html("Cleared.");
+      }
+    })
+  });
+
+});
 
